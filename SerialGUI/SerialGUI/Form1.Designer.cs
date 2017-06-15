bc3103316d30556c892ca68f6a5bf9514c2a918c
scorpionipx <scorpionipx@gmail.com>
2017-06-15 18:27:16 +0300

    Add files via upload

---
+++ SerialGUI/SerialGUI/Form1.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SerialGUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.status_label = new System.Windows.Forms.Label();
+            this.dataTextBox = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.angle_label = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // status_label
+            // 
+            this.status_label.AutoSize = true;
+            this.status_label.Location = new System.Drawing.Point(23, 25);
+            this.status_label.Name = "status_label";
+            this.status_label.Size = new System.Drawing.Size(35, 13);
+            this.status_label.TabIndex = 0;
+            this.status_label.Text = "label1";
+            // 
+            // dataTextBox
+            // 
+            this.dataTextBox.Location = new System.Drawing.Point(188, 25);
+            this.dataTextBox.Name = "dataTextBox";
+            this.dataTextBox.Size = new System.Drawing.Size(584, 279);
+            this.dataTextBox.TabIndex = 1;
+            this.dataTextBox.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(943, 383);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Send";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(188, 361);
+            this.trackBar1.Maximum = 3600;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.RightToLeftLayout = true;
+            this.trackBar1.Size = new System.Drawing.Size(749, 45);
+            this.trackBar1.SmallChange = 18;
+            this.trackBar1.TabIndex = 3;
+            this.trackBar1.TickFrequency = 18;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // angle_label
+            // 
+            this.angle_label.AutoSize = true;
+            this.angle_label.Location = new System.Drawing.Point(943, 361);
+            this.angle_label.Name = "angle_label";
+            this.angle_label.Size = new System.Drawing.Size(35, 13);
+            this.angle_label.TabIndex = 4;
+            this.angle_label.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1191, 512);
+            this.Controls.Add(this.angle_label);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataTextBox);
+            this.Controls.Add(this.status_label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label status_label;
+        private System.Windows.Forms.RichTextBox dataTextBox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label angle_label;
+    }
+}
+
---
+++ SerialGUI/SerialGUI/Form1.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace SerialGUI
+{
+    public partial class Form1 : Form
+    {
+        SerialPort evb_5 = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            __openPort__("COM4");
+            angle_label.Text = "Angle: ";
+        }
+
+        private void __openPort__(String port)
+        {
+            evb_5.PortName = port;
+            evb_5.BaudRate = 9600;
+            evb_5.Parity = Parity.None;
+            evb_5.DataBits = 8;
+            evb_5.StopBits = StopBits.One;
+            evb_5.DataReceived += new SerialDataReceivedEventHandler(port_DataReceived);
+            evb_5.Open();
+        }
+
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.dataTextBox.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.dataTextBox.Text += text;
+            }
+        }
+
+        private void __updateDataTextBox__(String data)
+        {
+            dataTextBox.Text += data;
+        }
+
+        private void port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            // Show all the incoming data in the port's buffer
+            // Console.WriteLine(evb_5.ReadExisting());
+
+            String txt = evb_5.ReadExisting().ToString();
+            SetText(txt);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int angle = trackBar1.Value;
+            int command_id = 1;
+            int data_byte_1;
+            int data_byte_2;
+
+            byte[] command_id_as_byte = BitConverter.GetBytes(command_id);
+
+            data_byte_1 = angle >> 8;
+            data_byte_2 = angle & 0xFF;
+
+            byte[] data_byte_1_as_byte = BitConverter.GetBytes(data_byte_1);
+            byte[] data_byte_2_as_byte = BitConverter.GetBytes(data_byte_2);
+
+            evb_5.Write(command_id_as_byte, 0, 1);
+            evb_5.Write(data_byte_1_as_byte, 0, 1);
+            evb_5.Write(data_byte_2_as_byte, 0, 1);
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            int angle = trackBar1.Value;
+
+            angle_label.Text = "Angle: " + (angle/10).ToString() + "." + (angle%10).ToString() + "°";
+
+            int command_id = 1;
+            int data_byte_1;
+            int data_byte_2;
+
+            byte[] command_id_as_byte = BitConverter.GetBytes(command_id);
+
+            data_byte_1 = angle >> 8;
+            data_byte_2 = angle & 0xFF;
+
+            byte[] data_byte_1_as_byte = BitConverter.GetBytes(data_byte_1);
+            byte[] data_byte_2_as_byte = BitConverter.GetBytes(data_byte_2);
+
+            evb_5.Write(command_id_as_byte, 0, 1);
+            evb_5.Write(data_byte_1_as_byte, 0, 1);
+            evb_5.Write(data_byte_2_as_byte, 0, 1);
+        }
+    }
+}
